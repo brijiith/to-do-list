3b0b1e90fe788a8e633a1a768ec2a8501e4bed8d
brijiith <brijith@neoito.com>
2022-01-14 15:53:51 +0530

    front console

--- server/index.js
+++ server/index.js
@@ -1,68 +1,76 @@
-const fs = require('fs');
-const http=require('http')
-const port=3000
-const server= http.createServer(function(req,res){
+const fs = require("fs");
+const http = require("http");
+const port = 3000;
+const server = http.createServer(function (req, res) {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000, // 30 days
+    /** add other headers as per requirement */
+  };
 
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Max-Age': 2592000, // 30 days
-        /** add other headers as per requirement */
-      };
-     
-      if (req.method === 'OPTIONS') {
-        res.writeHead(204, headers);
-        res.end();
-        return;
-      }
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
 
-    console.log('request was made:'+ req.url)
-    if(req.url === '/home' || req.url === '/'){
-        console.log(req.body);
+  console.log("request was made:" + req.url);
+  if (req.url === "/home" || req.url === "/") {
+    // console.log(req.body);
 
-        var body = ''
-        req.on('data', function(data) {
-          body += data
-          console.log('Partial body: ' + body)
-
-          fs.appendFile('returnValue.txt', '\n' + body, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-          });
-          
-        })
-        req.on('end', function() {
-          console.log('Body: ' + body)
-          const data = JSON.parse(body);
-        //   res.writeHead(200, {'Content-Type': 'text/html'})
-        //   res.end('post received')
-        })
-
-
-        res.writeHead(200, headers)
-         res.write(`{"text": "Hello world!!!"}`)
-        res.end()
-    }
-    else  if(req.url === '/list'){
-
-        res.writeHead(200, headers)
-        fs.readFile('returnValue.txt', (err, data) => {
+    var body = "";
+    const todos = [];
+    req.on("data", function (val) {
+      body += val;
+      // console.log("Partial body: " + body);
+      fs.readFile("returnValue.txt", function (err, data) {
+        let parsedData;
+        if(data.length===0) parsedData=[];
+        else{
+         parsedData=JSON.parse(data);
+        }
+        parsedData.push(JSON.parse(body));
+        // console.log('<<<<<<<',JSON.parse(body));
+        // todos.push(JSON.parse(data),);
+        // todos.push(JSON.parse(body));
+        // const todoString=JSON.stringify(todos);
+        // console.log('......',todoString);
+        fs.writeFile('returnValue.txt','\n'+JSON.stringify(parsedData),(err)=>{
           if (err) throw err;
-        
-          console.log(data.toJSON());
-          res.write(data)
-        res.end()
-      })
-      
-        
-        // res.headersSent({})
-    }
-   
-})
-server.listen(port,function(error) {
-    if (error){
-console.log('oops,wrong!!',error)
-    } else{
-console.log('server is active'+port)
-    }
-})
+          console.log("Saved!");
+        });
+        // const myJSON = JSON.parse(body);
+      });
+    });
+    req.on("end", function () {
+      console.log("Body: " + body);
+      const data = JSON.parse(body);
+      //   res.writeHead(200, {'Content-Type': 'text/html'})
+      //   res.end('post received')
+    });
+
+    res.writeHead(200, headers);
+    res.write(`{"text": "Hello world!!!"}`);
+    res.end();
+  } else if (req.url === "/list") {
+    res.writeHead(200, headers);
+    fs.readFile("returnValue.txt", (err, data) => {
+      if (err) throw err;
+      const payload = data.toString();
+      // payload = ['+ payload + ']
+      console.log(data.toString());
+      res.write(payload);
+      res.end();
+    });
+
+    // res.headersSent({})
+  }
+});
+server.listen(port, function (error) {
+  if (error) {
+    console.log("oops,wrong!!", error);
+  } else {
+    console.log("server is active" + port);
+  }
+});
